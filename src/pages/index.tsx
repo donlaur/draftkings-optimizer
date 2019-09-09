6fd79975c1b7bcff3fd0e6ff8d2a7d98f00f12a3
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-09-09 01:30:55 -0400

    Add background-color white to body

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,21 @@ import { Main } from '../layouts/main'
 export default function IndexPage() {
 	return (
 		<Main>
-			'test'
+			Est aftertaste barista, kopi-luwak robusta, roast dripper cortado a whipped. Extra brewed cortado mocha siphon froth grounds. Foam, id single shot extra sit cream filter. Blue mountain aged body roast ristretto percolator acerbic whipped bar latte. Filter redeye half and half arabica redeye, aroma, cup wings in eu spoon crema.
+
+At trifecta, doppio, eu dripper filter sugar fair trade. Carajillo est, qui, macchiato, whipped fair trade mug extra instant plunger pot flavour instant. Iced ristretto doppio, crema robusta plunger pot percolator aftertaste. Doppio roast, black, skinny cinnamon carajillo siphon espresso caramelization strong white. Café au lait shop, cappuccino strong kopi-luwak skinny cream cortado.
+
+Latte extraction, mug plunger pot americano carajillo caffeine cinnamon decaffeinated white. Eu so aged breve, variety, turkish doppio variety to go aftertaste. To go caffeine, blue mountain extra, dripper brewed latte skinny medium cortado brewed extraction. Half and half a robusta, frappuccino cup milk barista lungo. Coffee black, single origin saucer fair trade americano mocha caramelization.
+
+Skinny crema qui ristretto, latte espresso café au lait espresso sugar turkish mazagran redeye. Instant, cup plunger pot so frappuccino to go macchiato. Carajillo, steamed milk filter macchiato, dripper instant turkish affogato latte grinder steamed. Galão turkish extraction, acerbic est, café au lait, doppio filter grinder cinnamon breve medium. Filter acerbic, saucer, cup, cup coffee, americano, french press single origin turkish single origin java.
+
+Et white, seasonal coffee, blue mountain robusta spoon sweet fair trade ut caffeine. French press cortado, pumpkin spice carajillo saucer macchiato beans medium. Extraction turkish aged cinnamon, chicory espresso irish sweet siphon blue mountain. Half and half ristretto at bar percolator con panna body instant. Mazagran, grinder, dark, to go et body aged seasonal cappuccino plunger pot single origin.
+
+Robusta macchiato latte siphon, frappuccino, flavour crema aroma espresso foam irish. Aged whipped single shot sugar body cinnamon galão. Crema, eu, foam flavour crema cappuccino cortado aftertaste filter. At, americano aroma, irish, mazagran medium robust cinnamon shop. Single origin filter, cup breve, aged siphon to go roast doppio saucer acerbic latte.
+
+Ut, aroma mazagran sweet instant con panna robusta. Caramelization half and half spoon robust cortado plunger pot aftertaste. Brewed, irish espresso, coffee white coffee so roast. Café au lait, so doppio siphon percolator grinder kopi-luwak. Coffee kopi-luwak espresso, sugar froth aromatic eu sugar bar coffee.
+
+Saucer milk a, decaffeinated et frappuccino et flavour acerbic grounds latte. Instant milk in aromatic decaffeinated aged saucer ut rich iced. Wings, a cup a froth pumpkin spice plunger pot. Macchiato aged instant, decaffeinated aftertaste froth beans espresso galão instant. And rich, turkish, et mocha strong caramelization instant so ristretto flavour crema.
 		</Main>
 	)
 }
