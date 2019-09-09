1333bfb3aceee8bcd0737439722b94d46cbe75ec
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-09-09 01:08:52 -0400

    Add aside-nav

---
+++ src/layouts/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Aside } from '../components/global/aside/aside-nav';
+
+export function Main({ children }) {
+    return (
+        <>
+            <Aside/>
+
+            <main>
+                <div className="container">
+                    {children}
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import 'spectre.css/dist/spectre.min.css';
+import '../styles/styles.scss';
+
+import { Main } from '../layouts/main'
+
+export default function IndexPage() {
+	return (
+		<Main>
+			'test'
+		</Main>
+	)
+}
