29d7903c6692e9bdd1bd3baef50f02415a296eef
Mark <mjimenez@habaneroconsulting.com>
2019-10-10 16:00:30 -0400

    Fix dropdown icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,8 +86,11 @@ export default function IndexPage() {
 									})} />
 									<button className="input-dropdown__button" {...getToggleButtonProps({
 										disabled: isLoadingContests
-									})}>down</button>
-									{isOpen              
+									})}>
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="chevron-down"><rect width="24" height="24" opacity="0"/><path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z"/></g></g></svg>
+										Down
+									</button>
+									{isOpen
 										? (
 											<ul className="input-dropdown__list" {...getMenuProps()}>
 												{contests
