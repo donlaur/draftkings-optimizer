a0ca8683f8357f662bde67c1a96bbb5de3372c36
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-10-10 09:55:38 -0400

    Add SEO

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -4,10 +4,12 @@ import { Aside } from '../components/global/aside-nav';
 import 'normalize.css';
 import 'bootstrap/scss/bootstrap-grid.scss';
 import '../styles/styles.scss';
+import SEO from '../components/global/seo';
 
 export function Main({ children }) {
     return (
         <>
+            <SEO />
             <Aside/>
 
             <main>
