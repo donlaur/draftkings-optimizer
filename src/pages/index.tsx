f2a858b1a377e5f047c83d1070935afaa7960abf
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-10-11 08:29:53 -0400

    Add close button to input-dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,8 @@ interface IContestResponse {
 	groups: IGroup[]
 }
 
-const API = "https://evening-brushlands-00691.herokuapp.com";
+// const API = "https://evening-brushlands-00691.herokuapp.com";
+const API = "http://127.0.0.1:5000";
 
 export default function IndexPage() {
 	const [lineups, setLineups] = useState<ILineup[]>([]);
@@ -76,6 +77,8 @@ export default function IndexPage() {
 								getItemProps,
 								inputValue,
 								isOpen,
+								clearSelection,
+								selectedItem,
 								highlightedIndex,
 							}) => (
 								<div className={`input-dropdown ${isLoadingContests ? 'input-dropdown--disabled' : ''}`}>
@@ -84,12 +87,22 @@ export default function IndexPage() {
 										placeholder: isLoadingContests ? "Grabbing contests..." : "Search contest by ID or name",
 										disabled: isLoadingContests
 									})} />
-									<button className="input-dropdown__button" {...getToggleButtonProps({
-										disabled: isLoadingContests
-									})}>
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="chevron-down"><rect width="24" height="24" opacity="0"/><path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z"/></g></g></svg>
-										Down
-									</button>
+									{inputValue ? (
+										<button className="input-dropdown__button" 
+											onClick={clearSelection}
+											aria-label="clear selection"
+											>
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="close"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg>
+											Clear selection
+										</button>
+									) : (
+										<button className="input-dropdown__button" {...getToggleButtonProps({
+											disabled: isLoadingContests
+										})}>
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="chevron-down"><rect width="24" height="24" opacity="0"/><path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z"/></g></g></svg>
+											Down
+										</button>
+									)}
 									{isOpen
 										? (
 											<ul className="input-dropdown__list" {...getMenuProps()}>
