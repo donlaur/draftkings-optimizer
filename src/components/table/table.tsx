b0ba82f853c45a8c1d9c251ce029b7a7b5aab122
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-10-12 11:25:47 -0400

    Add lock option to table Changed table to table element

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -9,69 +9,75 @@ interface ITableProps {
 export function Table( { data }: ITableProps) {
 	return (
 		<div className="table-wrapper">
-			<div className="table" role="table">
-				<div className="table__row table__row--header">
+			<table className="table" role="table">
+				<tbody>
+				<tr className="table__row table__row--header">
 					{/* <div>ID</div> */}
-					<div className="table__column table__column--first-name">First name</div>
-					<div className="table__column table__column--last-name">Last name</div>
-					<div className="table__column">Team</div>
-					<div className="table__column">Positions</div>
-					<div className="table__column text-align-right">Salary</div>
-					<div className="table__column text-align-right">FPPG</div>
-					{/* <div className="table__column text-align-right"></div> */}
+					<th className="table__cell table__cell--lock"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="lock"><rect width="24" height="24" opacity="0"/><path d="M17 8h-1V6.11a4 4 0 1 0-8 0V8H7a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-8a3 3 0 0 0-3-3zm-7-1.89A2.06 2.06 0 0 1 12 4a2.06 2.06 0 0 1 2 2.11V8h-4zM18 19a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-8a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1z"/><path d="M12 12a3 3 0 1 0 3 3 3 3 0 0 0-3-3zm0 4a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"/></g></g></svg></th>
+					<th className="table__cell table__cell--first-name">First name</th>
+					<th className="table__cell table__cell--last-name">Last name</th>
+					<th className="table__cell">Positions</th>
+					<th className="table__cell">Team</th>
+					<th className="table__cell text-align-right">Salary</th>
+					<th className="table__cell text-align-right">FPPG</th>
+					{/* <div className="table__cell text-align-right"></div> */}
 					{/* {data.some((lineup) => lineup.players.some((player) => player.gameInfo)) ? (
 						<div>Game info</div>
 					) : <></>} */}
-				</div>
+				</tr>
 
 				{data.length !== undefined ? (
 					data.map((player: IDraftKingsResponse, i) => (
-						<div className={`table__row ${player.status !== 'None' ? `table__row--${player.status}` : ''}`} key={i}>
+						<tr className={`table__row ${player.status !== 'None' ? `table__row--${player.status}` : ''}`} key={i}>
 							{/* <div>{player.id}</div> */}
-							<div className="table__column table__column--first-name">{player.firstName}</div>
-							<div className="table__column table__column--last-name">{player.lastName}</div>
-							<div className="table__column">{player.teamAbbreviation}</div>
-							<div className="table__column">{player.position}</div>
-							<div className="table__column text-align-right">{player.salary}</div>
-							<div className="table__column text-align-right">{player.draftStatAttributes[0].value}</div>
-							{/* <div className="table__column text-align-right">Stats</div> */}
+							<td className="table__cell table__cell--lock">
+								<input className="checkbox" type="checkbox" name="" id="" disabled={player.status !== 'None'}/>
+							</td>
+							<td className="table__cell table__cell--first-name">{player.firstName}</td>
+							<td className="table__cell table__cell--last-name">{player.lastName}</td>
+							<td className="table__cell">{player.position}</td>
+							<td className="table__cell">{player.teamAbbreviation}</td>
+							<td className="table__cell text-align-right">{player.salary}</td>
+							<td className="table__cell text-align-right">{player.draftStatAttributes[0].value}</td>
+							{/* <div className="table__cell text-align-right">Stats</div> */}
 							{/* {player.gameInfo ? (
 								<div>{player.gameInfo}</div>
 							) : <></>} */}
-						</div>
+						</tr>
 					))
 				) : (
 					data.lineups.map((lineup: ILineup, i) => (
 						<>
 							{lineup.players.map((player) => (
-								<div className="table__row" key={player.id}>
+								<tr className="table__row" key={player.id}>
 									{/* <div>{player.id}</div> */}
-									<div className="table__column table__column--first-name">{player.firstName}</div>
-									<div className="table__column table__column--last-name">{player.lastName}</div>
-									<div className="table__column">{player.team}</div>
-									<div className="table__column">{player.positions}</div>
-									<div className="table__column text-align-right">{player.salary}</div>
-									<div className="table__column text-align-right">{player.fppg}</div>
-									{/* <div className="table__column text-align-right">Stats</div> */}
+									<td className="table__cell table__cell--lock">
+										<input className="checkbox" type="checkbox" name="" id=""/>
+									</td>
+									<td className="table__cell table__cell--first-name">{player.firstName}</td>
+									<td className="table__cell table__cell--last-name">{player.lastName}</td>
+									<td className="table__cell">{player.positions}</td>
+									<td className="table__cell">{player.team}</td>
+									<td className="table__cell text-align-right">{player.salary}</td>
+									<td className="table__cell text-align-right">{player.fppg}</td>
+									{/* <div className="table__cell text-align-right">Stats</div> */}
 									{/* {player.gameInfo ? (
 										<div>{player.gameInfo}</div>
 									) : <></>} */}
-								</div>
+								</tr>
 							))}
 
-							<div className="table__row table__row--total">
-								<div className="table__column">Total</div>
-								<div className="table__column"></div>
-								<div className="table__column"></div>
-								<div className="table__column"></div>
-								<div className="table__column text-align-right">{lineup.totalSalary}</div>
-								<div className="table__column text-align-right">{lineup.totalFppg}</div>
-								{/* <div className="table__column text-align-right"></div> */}
-							</div>
+							<tr className="table__row table__row--total">
+								<td className="table__cell" colSpan={5}>Total</td>
+								<td className="table__cell text-align-right">{lineup.totalSalary}</td>
+								<td className="table__cell text-align-right">{lineup.totalFppg}</td>
+								{/* <div className="table__cell text-align-right"></div> */}
+							</tr>
 						</>
 					))
 				)}
-			</div>
+				</tbody>
+			</table>
 		</div>
 	)
 }
