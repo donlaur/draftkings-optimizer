0b634fa0bf397bde1be4c24eb52fdb301cbc2d5d
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-10-16 00:20:15 -0400

    Format salary to currency

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -32,6 +32,7 @@ export function Table( { data, players, lockedPlayers, setLockedPlayers }: ITabl
 					<tr className="table__row table__row--header">
 						{/* <div>ID</div> */}
 						<th className="table__cell table__cell--lock"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="lock"><rect width="24" height="24" opacity="0"/><path d="M17 8h-1V6.11a4 4 0 1 0-8 0V8H7a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-8a3 3 0 0 0-3-3zm-7-1.89A2.06 2.06 0 0 1 12 4a2.06 2.06 0 0 1 2 2.11V8h-4zM18 19a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-8a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1z"/><path d="M12 12a3 3 0 1 0 3 3 3 3 0 0 0-3-3zm0 4a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"/></g></g></svg></th>
+						{/* <th className="table__cell"></th> */}
 						<th className="table__cell table__cell--first-name">First name</th>
 						<th className="table__cell table__cell--last-name">Last name</th>
 						<th className="table__cell">Positions</th>
@@ -51,13 +52,14 @@ export function Table( { data, players, lockedPlayers, setLockedPlayers }: ITabl
 									<td className="table__cell table__cell--lock">
 										{player.status !== 'O' ? (
 											<input className="checkbox" type="checkbox" onChange={onChange} value={player.playerId}/>
-										): <></>}
+											): <></>}
 									</td>
-									<td className="table__cell table__cell--first-name">{player.firstName}</td>
-									<td className="table__cell table__cell--last-name">{player.lastName}</td>
+									{/* <td className="table__cell">{player.status !== 'None' ? player.status : ''}</td> */}
+									<td className="table__cell">{player.firstName}</td>
+									<td className="table__cell">{player.lastName}</td>
 									<td className="table__cell">{player.position}</td>
 									<td className="table__cell">{player.teamAbbreviation}</td>
-									<td className="table__cell text-align-right">{player.salary}</td>
+									<td className="table__cell text-align-right">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(player.salary)}</td>
 									<td className="table__cell text-align-right">{player.draftStatAttributes[0].value}</td>
 									{/* <div className="table__cell text-align-right">Stats</div> */}
 									{/* {player.gameInfo ? (
@@ -81,7 +83,7 @@ export function Table( { data, players, lockedPlayers, setLockedPlayers }: ITabl
 										<td className="table__cell table__cell--last-name">{player.lastName}</td>
 										<td className="table__cell">{player.positions}</td>
 										<td className="table__cell">{player.team}</td>
-										<td className="table__cell text-align-right">{player.salary}</td>
+										<td className="table__cell text-align-right">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(player.salary)}</td>
 										<td className="table__cell text-align-right">{player.fppg}</td>
 										{/* <div className="table__cell text-align-right">Stats</div> */}
 										{/* {player.gameInfo ? (
@@ -93,7 +95,7 @@ export function Table( { data, players, lockedPlayers, setLockedPlayers }: ITabl
 							<tfoot>
 								<tr className="table__row table__row--total">
 									<td className="table__cell" colSpan={5}>Total</td>
-									<td className="table__cell text-align-right">{lineup.totalSalary}</td>
+									<td className="table__cell text-align-right">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(lineup.totalSalary)}</td>
 									<td className="table__cell text-align-right">{lineup.totalFppg}</td>
 								</tr>
 							</tfoot>
