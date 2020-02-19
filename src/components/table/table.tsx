56f43be8812f57b2a5865bee2b766e537a892012
Mark Jimenez <markjoeljimenez@gmail.com>
2020-02-19 00:19:07 -0500

    Style table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,87 +1,156 @@
 import React, { useEffect } from 'react';
 import { IResponse, ILineup } from '../interfaces/IApp';
-import { IDraftKingsResponse, IDraftKingsPlayer } from '../interfaces/IDraftKingsResponse';
+import {
+    IDraftKingsResponse,
+    IDraftKingsPlayer,
+} from '../interfaces/IDraftKingsResponse';
 
 interface ITableProps {
-	optimizedLineups: IResponse,
-	isOptimized?: boolean,
-	players: IDraftKingsPlayer[],
-	setPlayers?: React.Dispatch<React.SetStateAction<IDraftKingsPlayer[]>>
+    optimizedLineups: IResponse;
+    isOptimized?: boolean;
+    players: IDraftKingsPlayer[];
+    setPlayers?: React.Dispatch<React.SetStateAction<IDraftKingsPlayer[]>>;
 }
 
-export function Table( { optimizedLineups, isOptimized, players, setPlayers}: ITableProps) {
-	const onLock = (e: React.ChangeEvent<HTMLInputElement>) => {
-		if (e.currentTarget instanceof HTMLInputElement) {
-			const value = parseInt(e.currentTarget.closest('.table__row').getAttribute('id'));
+export function Table({
+    optimizedLineups,
+    isOptimized,
+    players,
+    setPlayers,
+}: ITableProps) {
+    const onLock = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget instanceof HTMLInputElement) {
+            const value = parseInt(
+                e.currentTarget.closest('.table__row').getAttribute('id')
+            );
 
-			const player = players.find((player) => player.id === value);
+            const player = players.find(player => player.id === value);
 
-			player.isLocked = !player.isLocked;
-			
-			setPlayers([...players]);
-		}
-	}
+            player.isLocked = !player.isLocked;
 
-	const onExclude = (e: React.MouseEvent<HTMLButtonElement>) => {
-		if (e.currentTarget instanceof HTMLButtonElement) {
-			const value = parseInt(e.currentTarget.closest('.table__row').getAttribute('id'));
+            setPlayers([...players]);
+        }
+    };
 
-			const player = players.find((player) => player.id === value);
-			
-			player.isExcluded = !player.isExcluded;
-			
-			setPlayers([...players]);
-		}
-	}
+    const onExclude = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (e.currentTarget instanceof HTMLButtonElement) {
+            const value = parseInt(
+                e.currentTarget.closest('.table__row').getAttribute('id')
+            );
 
-	return (
-		<div className="table-wrapper">
-			<table className="table">
-				<thead>
-					<tr className="table__row table__row--header">
-						<th className="table__cell table__cell--exclude"></th>
-						<th className="table__cell table__cell--lock"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="lock"><rect width="24" height="24" opacity="0"/><path d="M17 8h-1V6.11a4 4 0 1 0-8 0V8H7a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-8a3 3 0 0 0-3-3zm-7-1.89A2.06 2.06 0 0 1 12 4a2.06 2.06 0 0 1 2 2.11V8h-4zM18 19a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-8a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1z"/><path d="M12 12a3 3 0 1 0 3 3 3 3 0 0 0-3-3zm0 4a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"/></g></g></svg></th>
-						<th className="table__cell">First name</th>
-						<th className="table__cell">Last name</th>
-						<th className="table__cell">Positions</th>
-						<th className="table__cell">Team</th>
-						<th className="table__cell text-align-right">Salary</th>
-						<th className="table__cell text-align-right">FPPG</th>
-					</tr>
-				</thead>
-					{/* {players && !isOptimized ? (
-						<tbody className="table__tbody">
-							{players.map((player) => (
-								<tr className={`table__row ${player.isExcluded  ? `table__row--excluded` : player.status !== '' ? `table__row--${player.status}` : ''}`} key={player.id} id={`${player.id}`}>
-									<td className="table__cell table__cell--exclude">
-										{player.status !== 'O' ? (
-											<button onClick={onExclude}>
-												{player.isExcluded ? (
-														<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="plus"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2z"/></g></g></svg>
-													) : (
-														<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g data-name="Layer 2"><g data-name="close"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg>
-												)}
-												Exclude player
-											</button>
-										) : <></>}
-									</td>
-									<td className="table__cell table__cell--lock">
-										{player.status !== 'O' ? (
-											<input className="checkbox" type="checkbox" onChange={onLock} checked={player.isLocked}/>
-											): <></>}
-									</td>
-									<td className="table__cell">{player.first_name}</td>
-									<td className="table__cell">{player.last_name}</td>
-									<td className="table__cell">{player.position.name}</td>
-									<td className="table__cell">{player.team_id}</td>
-									<td className="table__cell text-align-right">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(player.draft.salary)}</td>
-									<td className="table__cell text-align-right">{player.points_per_contest}</td>
-								</tr>
-							))}
-						</tbody>
-					) : <></>} */}
+            const player = players.find(player => player.id === value);
 
-					{isOptimized ? optimizedLineups.lineups.map((lineup, i) => (
+            player.isExcluded = !player.isExcluded;
+
+            setPlayers([...players]);
+        }
+    };
+
+    return (
+        <div className="table-wrapper">
+            <table className="table">
+                <thead>
+                    <tr className="table__row table__row--header">
+                        <th className="table__cell table__cell--lock"></th>
+                        <th className="table__cell">Status</th>
+                        <th className="table__cell">First name</th>
+                        <th className="table__cell">Last name</th>
+                        <th className="table__cell">Positions</th>
+                        <th className="table__cell">Team</th>
+                        <th className="table__cell text-align-right">Salary</th>
+                        <th className="table__cell text-align-right">FPPG</th>
+                        <th className="table__cell table__cell--lock"></th>
+                    </tr>
+                </thead>
+                {players && !isOptimized ? (
+                    <tbody className="table__tbody">
+                        {players.map(player => (
+                            <tr
+                                className={`table__row ${
+                                    player.isExcluded
+                                        ? `table__row--excluded`
+                                        : player.status !== ''
+                                        ? `table__row--${player.status}`
+                                        : ''
+                                }`}
+                                key={player.id}
+                                id={`${player.id}`}
+                            >
+                                <td className="table__cell table__cell--lock">
+                                    {player.status !== 'O' ? (
+                                        <input
+                                            className="checkbox"
+                                            type="checkbox"
+                                            onChange={onLock}
+                                            checked={player.isLocked}
+                                        />
+                                    ) : (
+                                        <></>
+                                    )}
+                                </td>
+                                <td className="table__cell table__cell--center">
+                                    <span
+                                        className={`pill ${
+                                            player.status
+                                                ? `pill--${player.status}`
+                                                : 'pill--active'
+                                        }`}
+                                    >
+                                        {player.status
+                                            ? player.status
+                                            : 'Active'}
+                                    </span>
+                                </td>
+                                <td className="table__cell">
+                                    {player.first_name}
+                                </td>
+                                <td className="table__cell">
+                                    {player.last_name}
+                                </td>
+                                <td className="table__cell">
+                                    {player.position.name}
+                                </td>
+                                <td className="table__cell">{player.team}</td>
+                                <td className="table__cell text-align-right">
+                                    {new Intl.NumberFormat('en-US', {
+                                        style: 'currency',
+                                        currency: 'USD',
+                                        minimumFractionDigits: 0,
+                                    }).format(player.draft.salary)}
+                                </td>
+                                <td className="table__cell text-align-right">
+                                    {player.points_per_contest}
+                                </td>
+                                <td className="table__cell table__cell--lock">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="24"
+                                        height="24"
+                                    >
+                                        <g data-name="Layer 2">
+                                            <g data-name="more-vertical">
+                                                <rect
+                                                    width="24"
+                                                    height="24"
+                                                    transform="rotate(-90 12 12)"
+                                                    opacity="0"
+                                                />
+                                                <circle cx="12" cy="12" r="2" />
+                                                <circle cx="12" cy="5" r="2" />
+                                                <circle cx="12" cy="19" r="2" />
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                ) : (
+                    <></>
+                )}
+
+                {/* '{isOptimized ? optimizedLineups.lineups.map((lineup, i) => (
 						<React.Fragment key={i}>
 							<tbody className="table__tbody" key={i}>
 								{lineup.players.map((player, i) => {
@@ -122,8 +191,8 @@ export function Table( { optimizedLineups, isOptimized, players, setPlayers}: IT
 								</tr>
 							</tfoot>
 						</React.Fragment>
-					)) : <></>}
-			</table>
-		</div>
-	)
+					)) : <></>}' */}
+            </table>
+        </div>
+    );
 }
